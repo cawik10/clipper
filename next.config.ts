fea822c4c59c4bf9b4a963fc0dd5582086b7e9ee
cawik10 <mascawik10@gmail.com>
2026-09-24 18:52:54 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Tambahkan baris ini
   serverExternalPackages: ["child_process", "fs", "path", "os"],
   experimental: {
     serverActions: {
