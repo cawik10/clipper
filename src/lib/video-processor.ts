113629f1c5e5eb3bd809e1dab50b67599360bfec
cawik10 <mascawik10@gmail.com>
2026-09-24 20:23:02 +0700

    Update video-processor.ts

--- src/lib/video-processor.ts
+++ src/lib/video-processor.ts
@@ -84,6 +84,7 @@ export async function downloadVideo(
   const args = [
     url,
     "--cookies", path.join(process.cwd(), "youtube-cookies.txt"),
+    "--extractor-args", "youtube:player_client=android,web",
     "-o", outputTemplate,
     "--format", hasFFmpeg
       ? "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/best"
